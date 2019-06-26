56e4d92d565571fd52fbb452a26e380650ca938a
Bruno Weiler <brunoweiller@hotmail.com>
2019-06-25 22:28:20 -0300

    front

--- front/src/App.js
+++ front/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          fazer sidebar e deletar arquivos desnecessários.
         </p>
         <a
           className="App-link"
