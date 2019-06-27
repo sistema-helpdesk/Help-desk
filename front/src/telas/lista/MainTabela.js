8fe3d6dd19cfc3ca9e0ae1dbe1ccc03ff3cb4f58
Bruno Weiler <brunoweiller@hotmail.com>
2019-06-27 19:32:07 -0300

    Arrumando collapse

--- front/src/telas/lista/MainTabela.js
+++ front/src/telas/lista/MainTabela.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react';
 import Tabela from './Tabela';
 
 export default class MainTabela extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapseID: false,
-    }
-  }
   render() {
     return (
       <div className='container containerBorda' style={{ marginTop: 60, marginBottom: 60 }}>
         <div className='row'>
           <div className='col-sm-1'></div>
           <div className='col-sm-10 alinhandoEsquerda z-depth-3'>
-            <Tabela propzin='usando props' collapseID={this.state.collapseID}></Tabela>
+            <Tabela ></Tabela>
           </div>
           <div className='col-sm-1'></div>
         </div>
--- front/src/telas/lista/Tabela.js
+++ front/src/telas/lista/Tabela.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { MDBTable, MDBTableHead, MDBTableBody, MDBCollapse } from 'mdbreact';
+import React,{Component} from 'react';
+import { MDBTable, MDBTableHead, MDBTableBody, MDBCollapse, MDBBtn } from 'mdbreact';
 import Chamado from '../chamado/Chamado';
 
 
 //Transformar em classe
-export default function Tabela(props) {
+export default class Tabela extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapseID: false,
+    }
+  }
   // let collapseID = false,
   // function toggleCollapse() {
   //   let colapsado = ''
@@ -17,60 +23,64 @@ export default function Tabela(props) {
   //   console.log(props.collapseID)
   // }
 
-  // function toggleCollapse() {
-  //   let colapsado = ''
-  //   if (this.state.collapseID === false) {
-  //     colapsado = true
-  //   } else {
-  //     colapsado = false
-  //   }
-  //   collapseID: colapsado
-  // };
+  toggleCollapse() {
+    let colapsado = ''
+    if (this.state.collapseID === false) {
+      colapsado = true
+    } else {
+      colapsado = false
+    }
+    this.setState({collapseID: colapsado})
+  };
+  render(){
 
-  return (
-    <div>
-      <MDBTable>
-        <MDBTableHead>
-          <tr>
-            <th>ID</th>
-            <th>Descrição / Solução</th>
-            <th>Situação</th>
-            <th>Responsável</th>
-          </tr>
-        </MDBTableHead>
-        <MDBTableBody>
-          <tr>
-            <th>1</th>
-            <th><button >Ver</button></th>
-            <th>Em andamento</th>
-            <th>Bruno</th>
-          </tr>
-          <tr>
-            <th>2</th>
-            <th><button>Ver</button></th>
-            <th>Em andamento</th>
-            <th>Marcio</th>
-          </tr>
-          <tr>
-            <th>3</th>
-            <th><button>Ver</button></th>
-            <th>Em andamento</th>
-            <th>Rodr</th>
-          </tr>
-          <tr>
-            <th>4</th>
-            <th><button>{/*props.propzin*/} Ver</button></th>
-            <th>Em andamento</th>
-            <th>Marcel</th>
-          </tr>
-        </MDBTableBody>
-      </MDBTable>
-      <MDBCollapse isOpen={true}>
-
-        {/* {this.state.erro && <div className="alert alert-danger">{this.state.erro}</div>} */}
-        <Chamado empresa='E' setor='S' descricao='D'></Chamado>
-
-      </MDBCollapse>
-    </div>
-  );
+    return (
+      <div>
+        <MDBTable>
+          <MDBTableHead>
+            <tr>
+              <th>ID</th>
+              <th>Descrição / Solução</th>
+              <th>Situação</th>
+              <th>Responsável</th>
+            </tr>
+          </MDBTableHead>
+          <MDBTableBody>
+            <tr>
+              <th>1</th>
+              <th><button onClick={() => this.toggleCollapse()} >Ver</button></th>
+              <th>Em andamento</th>
+              <th>Bruno</th>
+            </tr>
+            <tr>
+              <th>2</th>
+              <th><button onClick={() => this.toggleCollapse()} >Ver</button></th>
+              <th>Em andamento</th>
+              <th>Marcio</th>
+            </tr>
+            <tr>
+              <th>3</th>
+              <th><button onClick={() => this.toggleCollapse()} >Ver</button></th>
+              <th>Em andamento</th>
+              <th>Rodr</th>
+            </tr>
+            <tr>
+              <th>4</th>
+              <th><button onClick={() => this.toggleCollapse()} >{/*props.propzin*/} Ver</button></th>
+              <th>Em andamento</th>
+              <th>Marcel</th>
+            </tr>
+          </MDBTableBody>
+        </MDBTable>
+        <MDBCollapse isOpen={this.state.collapseID}>
+  
+          {/* {this.state.erro && <div className="alert alert-danger">{this.state.erro}</div>} */}
+          <Chamado empresa='E' setor='S' descricao='D'></Chamado>
+            <MDBBtn color="primary">Salvar</MDBBtn>
+  
+        </MDBCollapse>
+      </div>
+    );
+  
+  }
 }
