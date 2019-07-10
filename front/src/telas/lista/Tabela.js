b0c5502084f9af6366a885348ae7f18266403c8e
Bruno Weiler <brunoweiller@hotmail.com>
2019-07-09 22:29:30 -0300

    Começando a manutenção do sistema

--- front/src/telas/lista/Tabela.js
+++ front/src/telas/lista/Tabela.js
@@ -8,20 +8,24 @@ export default class Tabela extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      descricaoVista: '',
+      solucaoVista: '',
       collapseID: false,
       empresa: 'E',
       setor: 'S',
       descricaoAtual: '',
-      solucaoAtual: null,
+      solucaoAtual: '',
       solucao: '',
       listaChamada: [],
       idAtual: null,
       erroAtual: null,
-      empresaAtual:null,
-      setorAtual:null,
-      statusAtual: 'Concluido'
+      empresaAtual: null,
+      setorAtual: null,
+      statusAtual: 'Concluido',
+      listaSetores: []
     }
   }
+
   componentDidMount() {
     axios.get('http://localhost:8080/chamados')
       .then(resposta => {
@@ -40,13 +44,30 @@ export default class Tabela extends Component {
         alert('Deu errado!')
         console.log(resposta)
       })
+      axios.get('http://localhost:8080/setores')
+        .then(resposta => {
+          let data = null
+          //se deu certo:
+          data = resposta.data
+          // data.map(linha => {
+  
+          // });
+          // console.log(data)
+          this.setState({ listaSetores: data })
+          console.log(this.state.listaSetores)
+        })
+        .catch(resposta => {
+          //se deu errado:
+          alert('Deu errado!')
+          console.log(resposta)
+        })
   }
 
 
   chamadoEditar() {
-    if( !this.state.solucao ){
+    if (!this.state.solucao) {
       alert('Preencha uma solução!')
-    } else{
+    } else {
       axios.put('http://localhost:8080/chamados/' + this.state.idAtual, {
         id: this.state.idAtual,
         nEmpresa: this.state.empresaAtual,
@@ -66,18 +87,25 @@ export default class Tabela extends Component {
 
   toggleCollapse(idzin, descr, solu, err, stat, empr, set) {
     let colapsado = ''
+    // let solucaoTemp = ''
     if (this.state.collapseID === false) {
       colapsado = true
     } else {
       colapsado = false
     }
+    //Feito para fazer o campo 'solução Atual:'
+    // if (!solu) {
+    //   solucaoTemp = ''
+    // } else {
+    //   solucaoTemp = 'Solução atual: ' + solu
+    // }
     // console.log(this.state.listaChamada[1])
     this.setState({ collapseID: colapsado, idAtual: idzin, descricaoAtual: descr, solucaoAtual: solu, erroAtual: err, status: stat, empresaAtual: empr, setorAtual: set })
   };
 
   render() {
     return (
-      <div>
+      <div style={{ paddingBottom: 30 }}>
         <MDBTable>
           <MDBTableHead>
             <tr>
@@ -85,8 +113,9 @@ export default class Tabela extends Component {
               <th>Empresa</th>
               <th>Setor</th>
               <th className='alinhandoCentro'>Descrição / Solução</th>
+              <th className='alinhandoCentro'>Editar</th>
               <th>Situação</th>
-              {/* <th>Responsável</th> */}
+              <th>Responsável</th>
             </tr>
           </MDBTableHead>
           <MDBTableBody>
@@ -95,23 +124,31 @@ export default class Tabela extends Component {
                 <th>{chamados.id}</th>
                 <th>{chamados.nEmpresa}</th>
                 <th>{chamados.nSetor}</th>
-                <th className='alinhandoCentro'><button onClick={() => this.toggleCollapse(chamados.id, chamados.descricao, chamados.solucao, chamados.erro, chamados.status, chamados.nEmpresa, chamados.nSetor)} >Ver</button></th>
+                <th className='alinhandoCentro'><button onClick={() => this.setState({ descricaoVista: chamados.descricao, solucaoVista: chamados.solucao===null ? "" : chamados.solucao })} >Ver</button></th>
+                <th className='alinhandoCentro'><button onClick={() => this.toggleCollapse(chamados.id, chamados.descricao, chamados.solucao, chamados.erro, chamados.status, chamados.nEmpresa, chamados.nSetor)} >Editar</button></th>
                 <th>{chamados.status}</th>
+                <th>respo</th>
+                {/* {chamados.setor} */}
               </tr>
             )}
           </MDBTableBody>
         </MDBTable>
-        <MDBCollapse isOpen={this.state.collapseID}>
+        <MDBCollapse isOpen={this.state.collapseID} style={{paddingBottom:30}}>
 
           {/* {this.state.erro && <div className="alert alert-danger">{this.state.erro}</div>} */}
           {/* <MDBInput type="textarea" label="Empresa" rows="1" value={this.state.empresa} disabled></MDBInput>
           <MDBInput type="textarea" label="Setor" rows="1" value={this.state.setor} disabled></MDBInput> */}
           <MDBInput type="textarea" label="Descrição" rows="5" value={this.state.descricaoAtual} disabled></MDBInput>
           <MDBInput type="textarea" label="Solução" rows="5" onChange={(event => this.setState({ solucao: event.target.value }))} ></MDBInput>
-          <p>Solução atual: {this.state.solucaoAtual}</p>
+          {/* <p className='blue-text'>{this.state.solucaoAtual}</p> */}
           <MDBBtn color="primary" onClick={() => this.chamadoEditar()}>Salvar</MDBBtn>
-
         </MDBCollapse>
+        <hr></hr>
+        <p>Descrição: </p>
+        <textarea value={this.state.descricaoVista} style={{ width: 920, height: 100 }} disabled></textarea>
+        <br></br>
+        <p>Solução: </p>
+        <textarea label="Solução" value={this.state.solucaoVista} style={{ width: 920, height: 100 }} disabled></textarea>
       </div>
     );
 
